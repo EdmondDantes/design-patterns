7ae7192241222f9826b3a687cab7709e72348ab8
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-24 15:04:22 +0300

    + ScopeControl

---
+++ src/ScopeControl/ScopeContextAwareInterface.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\DesignPatterns\ScopeControl;
+
+interface ScopeContextAwareInterface
+{
+    public function getScopeContext(): ScopeContextInterface;
+}
---
+++ src/ScopeControl/ScopeManagerInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\DesignPatterns\ScopeControl;
+
+/**
+ * ## ScopeManagerInterface
+ * ScopeManager manages the visibility rules repository and visibility settings.
+ */
+interface ScopeManagerInterface
+{
+
+}
