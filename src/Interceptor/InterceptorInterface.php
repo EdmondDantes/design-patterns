885b0664ae9da773548ad74712b95aa81c525c22
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-26 09:59:16 +0300

    + Docs for Interceptor + Docs for ExecutionPlan

--- src/Interceptor/InterceptorInterface.php
+++ src/Interceptor/InterceptorInterface.php
@@ -5,5 +5,8 @@ namespace IfCastle\DesignPatterns\Interceptor;
 
 interface InterceptorInterface
 {
+    /**
+     * Called before the execution of the target.
+     */
     public function intercept(InterceptorPipelineInterface $pipeline): void;
 }
