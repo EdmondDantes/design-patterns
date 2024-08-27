09f6a7313721987a7cdafe2b493593b63f338738
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-27 13:09:07 +0300

    + BeforeAfterHandlersAwareInterface

---
+++ src/ExecutionPlan/BeforeAfterHandlersAwareInterface.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\DesignPatterns\ExecutionPlan;
+
+interface BeforeAfterHandlersAwareInterface
+{
+    public function getBeforeAfterHandlers(): BeforeAfterActionInterface;
+}
