b4dd743b3edd8d3b9ef4fdc3881d9b5918175794
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-30 23:22:33 +0300

    + Pool pattern DecoratorAsProxy

---
+++ src/Pool/DecoratorAsProxy.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\DesignPatterns\Pool;
+
+use IfCastle\DI\DisposableInterface;
+
+final class DecoratorAsProxy implements DecoratorInterface, DisposableInterface
+{
+    private \WeakReference|null $pool;
+    
+    public function __construct(private object|null $originalObject, PoolInterface $pool)
+    {
+        $this->pool                 = \WeakReference::create($pool);
+    }
+    
+    #[\Override]
+    public function getOriginalObject(): object
+    {
+        return $this->originalObject;
+    }
+    
+    #[\Override]
+    public function dispose(): void
+    {
+        $pool                       = $this->pool?->get();
+        $originalObject             = $this->originalObject;
+        $this->pool                 = null;
+        $this->originalObject       = null;
+        
+        $pool?->return($originalObject);
+    }
+}
