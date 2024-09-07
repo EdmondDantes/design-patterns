256eee367c2aecc47b99d4d8262deca2c72fe36b
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-09-07 11:10:54 +0300

    + BeforeAfterExecutorAwareInterface

---
+++ src/ExecutionPlan/BeforeAfterExecutorAwareInterface.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\DesignPatterns\ExecutionPlan;
+
+interface BeforeAfterExecutorAwareInterface
+{
+    public function getBeforeAfterExecutor(): BeforeAfterExecutorInterface;
+}
