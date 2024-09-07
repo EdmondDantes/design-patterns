9c92b8a7149a38d63c9031ab9ee0892ddc4419c7
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-09-07 11:40:40 +0300

    Extracted class instantiation logic from HandlerExecutorCallable to new HandlerExecutorWithClass class.

--- src/ExecutionPlan/HandlerExecutorWithClass.php
+++ src/ExecutionPlan/HandlerExecutorWithClass.php
@@ -3,10 +3,14 @@ declare(strict_types=1);
 
 namespace IfCastle\DesignPatterns\ExecutionPlan;
 
-final class HandlerExecutorCallable implements HandlerExecutorInterface
+final class HandlerExecutorWithClass implements HandlerExecutorInterface
 {
     public function executeHandler(mixed $handler, string $stage): void
     {
+        if(is_string($handler) && class_exists($handler)) {
+            $handler = new $handler();
+        }
+        
         if(is_callable($handler)) {
             $handler($stage);
         }
