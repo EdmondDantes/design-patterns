e603f8dedca3c50adcdb1ca80b365c6892de8d9e
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-09-07 11:38:27 +0300

    Removed `protected` visibility modifier for `ContainerInterface` parameter in constructor of `HandlerExecutorWithResolver`.

--- src/ExecutionPlan/HandlerExecutorWithResolver.php
+++ src/ExecutionPlan/HandlerExecutorWithResolver.php
@@ -8,5 +8,5 @@ use IfCastle\DI\ResolverInterface;
 
 class HandlerExecutorWithResolver extends HandlerExecutorWithResolverAbstract
 {
-    public function __construct(protected ContainerInterface $container, protected ResolverInterface $resolver) {}
+    public function __construct(ContainerInterface $container, protected ResolverInterface $resolver) {}
 }
