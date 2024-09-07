f20459f97176f58a7d3547c26ac81d3f441776a2
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-09-07 10:48:30 +0300

    Refactored HandlerExecutorWithResolver to inherit from new HandlerExecutorWithResolverAbstract. Added HandlerExecutorWithResolverSetter class.

--- src/ExecutionPlan/HandlerExecutorWithResolver.php
+++ src/ExecutionPlan/HandlerExecutorWithResolver.php
@@ -3,38 +3,10 @@ declare(strict_types=1);
 
 namespace IfCastle\DesignPatterns\ExecutionPlan;
 
-use IfCastle\DI\AutoResolverInterface;
 use IfCastle\DI\ContainerInterface;
-use IfCastle\DI\DependencyInterface;
-use IfCastle\DI\DisposableInterface;
-use IfCastle\DI\InitializerInterface;
 use IfCastle\DI\ResolverInterface;
 
-final readonly class HandlerExecutorWithResolver implements HandlerExecutorInterface
+class HandlerExecutorWithResolver extends HandlerExecutorWithResolverAbstract
 {
-    public function __construct(private ContainerInterface $container, private ResolverInterface $resolver) {}
-    
-    #[\Override]
-    public function executeHandler(mixed $handler, string $stage): void
-    {
-        if($handler instanceof InitializerInterface) {
-            $handler                = $handler->executeInitializer($this->container);
-        }
-        
-        if($handler instanceof AutoResolverInterface) {
-            $handler->resolveDependencies($this->container);
-        } elseif ($handler instanceof DependencyInterface) {
-            $handler                = $this->resolver->resolveDependency($handler, $this->container);
-        }
-
-        try {
-            if($handler instanceof StageHandlerInterface) {
-                $handler->handleStage($stage);
-            }
-        } finally {
-            if($handler instanceof DisposableInterface) {
-                $handler->dispose();
-            }
-        }
-    }
+    public function __construct(protected ContainerInterface $container, protected ResolverInterface $resolver) {}
 }
