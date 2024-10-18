bf7add084e16d71073d94aeb879a1f90feb2960c
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-18 16:02:25 +0300

    + ImmutableTrait

--- src/Immutable/ImmutableTrait.php
+++ src/Immutable/ImmutableTrait.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace IfCastle\DesignPatterns\Immutable;
 
+use IfCastle\Exceptions\LogicalException;
+
 trait ImmutableTrait
 {
     protected bool $isImmutable     = false;
@@ -28,5 +30,27 @@ trait ImmutableTrait
         $cloned                     = clone $this;
         $cloned->isImmutable        = false;
         return $cloned;
+    }
+    
+    /**
+     * @throws LogicalException
+     */
+    protected function throwIfImmutable(): void
+    {
+        if($this->isImmutable) {
+            
+            $backtrace              = debug_backtrace(limit: 3);
+            $calledMethod           = 'unknown';
+            
+            if(isset($backtrace[2]['function'])) {
+                $calledMethod       = $backtrace[2]['function'];
+            }
+            
+            throw new LogicalException([
+                'template'  => 'This object {class} is immutable and cannot be modified. Called from {method}',
+                'class'     => static::class,
+                'method'    => $calledMethod
+            ]);
+        }
     }
 }
